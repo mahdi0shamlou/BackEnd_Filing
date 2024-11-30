a1deb41fe0d42c302c1c617fe3b08fc4b0d9716e
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-11-30 03:12:14 -0500

    working on route search

--- WebApp/search_engine.py
+++ WebApp/search_engine.py
@@ -14,23 +14,38 @@ def check_user_has_accses(user, mahal):
     print(mahal)
     return True
 
-@searchenign_bp.route('/Search', methods=['GET'])
+@searchenign_bp.route('/Search/Test', methods=['GET'])
 @jwt_required()
-def searhc_file():
+def search_engine():
+    try:
+        is_sell = request.args.get('type_files', default=1, type=int)
+        if is_sell == 1:
+            city = request.args.get('city', default=1, type=int)
+            type = request.args.get('type', default=21, type=int)
+            mahals = request.args.getlist('mahal[]', type=int)  # Get list of mahal IDs
+            price_from = request.args.get('price_from', type=int)
+            price_to = request.args.get('price_to', type=int)
+            rent_from = request.args.get('rent_from', type=int)
+            rent_to = request.args.get('rent_to', type=int)
+            meter_from = request.args.get('meter_from', type=int)
+            meter_to = request.args.get('meter_to', type=int)
+            sort_from = request.args.get('sort', 1, type=int)
+            otagh = request.args.get('otagh', type=int)
+            make = request.args.getlist('make[]', type=int)  # Get list of mkae years
+            desck = request.args.get('desck', type=str)
 
-    type_of_search = request.form.get('type', 1, type=int)
-    city = request.form.get('city', 1, type=int)
-    mahal = request.form.get('mahal', 1, type=int)
+            current_user = get_jwt_identity()
+            user_phone = current_user['phone']
 
-    current_user = get_jwt_identity()
-    user_phone = current_user['phone']
+            user = Users.query.filter_by(phone=user_phone).first()
 
-    # Query the 'users' table for a user with the matching phone number
-    user = Users.query.filter_by(phone=user_phone).first()
+            check_accses = check_user_has_accses(user, mahals)
+            if check_accses:
+                return jsonify({"message": "فایلینگ"}), 200
+            else:
+                return jsonify({"message": "شما به این منطقه دسترسی ندارید ."}), 403
 
-    check_accses = check_user_has_accses(user, mahal)
-    if check_accses:
-        return jsonify({"message": "فایلینگ"}), 200
-    else:
-        return jsonify({"message": "شما به این منطقه دسترسی ندارید ."}), 403
-
+        else:
+            pass
+    except Exception as e:
+        print(e)
