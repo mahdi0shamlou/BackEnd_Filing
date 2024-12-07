627ae3d2a25228a64e787937e6de343106fb8653
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-12-07 06:16:04 -0500

    working on classifications

--- WebApp/search_engine.py
+++ WebApp/search_engine.py
@@ -295,48 +295,9 @@ def search_engine_full_details():
 # mahal requests
 #-------------------------------------
 
-def get_user_neighborhoods_access_2(user_id):
-    try:
-        # ساخت کوئری با select
-        user_classifications = select(UserAccess.classifictions_id).where(
-            UserAccess.user_id == user_id
-        ).scalar_subquery()
-
-        # کوئری اصلی
-        neighborhoods = db.session.query(
-            Neighborhood.name,
-            Classification.name.label('classification_name'),
-            ClassificationNeighborhood.type,
-            ClassificationNeighborhood.neighborhood_id
-        ).join(
-            ClassificationNeighborhood,
-            Neighborhood.id == ClassificationNeighborhood.neighborhood_id
-        ).join(
-            Classification,
-            ClassificationNeighborhood.classifiction_id == Classification.id
-        ).filter(
-            Classification.id.in_(user_classifications)
-        ).all()
-
-        # تبدیل نتایج به دیکشنری
-        result = []
-        for n in neighborhoods:
-            result.append({
-                'neighborhood_name': n.name,
-                'classification_name': n.classification_name,
-                'neighborhood_id': n.neighborhood_id,
-                'access_type': n.type
-            })
-        return result
-
-    except Exception as e:
-        print(f"Error in get_user_neighborhoods_access_2: {str(e)}")
-        return None
-
-
 @searchenign_bp.route('/Search/User/Access', methods=['GET'])
 @jwt_required()
-def Users_Access():
+def users_access():
     try:
         current_user = get_jwt_identity()
         user_phone = current_user['phone']
@@ -348,7 +309,7 @@ def Users_Access():
         # دریافت دسته‌بندی‌های کاربر با استفاده از join
         classifications = db.session.query(Classification)\
             .join(UserAccess, UserAccess.classifictions_id == Classification.id)\
-            .filter(UserAccess.user_id == user.id)\
+            .filter(UserAccess.user_id == 2)\
             .all()
 
 
@@ -377,3 +338,72 @@ def Users_Access():
             'status': 'error',
             'message': str(e)
         }), 500)
+
+
+@searchenign_bp.route('/Search/User/Class', methods=['GET'])
+@jwt_required()
+def users_access_class():
+    try:
+        # Parse JSON body
+        request_data = request.get_json()
+        classification_id = request_data.get('class', 1) # 1 ,2 , 3, 4, 5, 6
+        """        
+        current_user = get_jwt_identity()
+        user_phone = current_user['phone']
+        user = Users.query.filter_by(phone=user_phone).first()
+
+        #results = get_user_neighborhoods_access_2(user.id)
+        #print(results)
+        """
+        neighborhoods = db.session.query(Neighborhood) \
+            .join(ClassificationNeighborhood,
+                  ClassificationNeighborhood.neighborhood_id == Neighborhood.id) \
+            .filter(ClassificationNeighborhood.classifiction_id == classification_id) \
+            .all()
+
+        # تبدیل نتایج به دیکشنری
+        results = []
+        for neighborhood in neighborhoods:
+            """
+            # دریافت نوع محله از جدول میانی
+            classification_neighborhood = ClassificationNeighborhood.query.filter_by(
+                classifiction_id=classification_id,
+                neighborhood_id=neighborhood.id
+            ).first()
+            """
+            results.append({
+                'value': neighborhood.id,
+                'label': neighborhood.name,
+                #'type': classification_neighborhood.type,
+                'date_created': neighborhood.date_created.strftime(
+                    '%Y-%m-%d %H:%M:%S') if neighborhood.date_created else None
+            })
+
+
+
+        # دریافت تایپ‌های منحصر به فرد
+        unique_types = db.session.query(ClassificationNeighborhood.type) \
+            .filter(ClassificationNeighborhood.classifiction_id == classification_id) \
+            .distinct() \
+            .all()
+
+        # تبدیل نتیجه به لیست ساده
+        types_list = [type_[0] for type_ in unique_types]
+        if results is None:
+            return make_response(jsonify({
+                'status': 'error',
+                'message': 'An error occurred while fetching data'
+            }), 500)
+
+        return make_response(jsonify({
+            'status': 'success',
+            'mahal_user_access': results,
+            'types_user_access' : types_list
+        }), 200)
+
+    except Exception as e:
+        return make_response(jsonify({
+            'status': 'error',
+            'message': str(e)
+        }), 500)
+
