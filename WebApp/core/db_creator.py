2272cd2fa0efabf907fce35466cdf03e324366a1
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-12-15 02:00:14 -0500

    Update some db_creators table

--- WebApp/core/db_creator.py
+++ WebApp/core/db_creator.py
@@ -368,9 +368,9 @@ class DatabaseManager:
         create_table_query = """
             CREATE TABLE IF NOT EXISTS User_Access (
                 id BIGINT(20) AUTO_INCREMENT PRIMARY KEY,
-                factor_id BIGINT(20) UNSIGNED NOT NULL,
+                factor_id BIGINT(20) NOT NULL,
                 user_id BIGINT(20) UNSIGNED NOT NULL,
-                classifictions_id BIGINT(20) UNSIGNED NOT NULL,
+                classifictions_id BIGINT(20)  NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 expired_at TIMESTAMP NOT NULL,
                 updated_at TIMESTAMP NULL DEFAULT NULL,
