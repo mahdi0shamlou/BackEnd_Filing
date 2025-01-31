e56983903fde50527a98d1526af0eb6bdbc21ac3
mahdi0shamlou <shamlouprogramer@gmail.com>
2025-01-31 03:42:23 -0500

    Update factor

--- WebApp/factor.py
+++ WebApp/factor.py
@@ -57,10 +57,7 @@ def get_factors():
 
     try:
         # پیدا کردن فاکتورهای مربوط به کاربر فعلی
-        factors = Factor.query.filter_by(user_id=user.id).all()
-        
-        # Add ordering by id in descending order
-        factors = factors.order_by(Factor.id.desc())
+        factors = Factor.query.filter_by(user_id=user.id).order_by(Factor.id.desc()).all()
 
         # تبدیل فاکتورها به فرمت جیسون
         factors_list = [{
